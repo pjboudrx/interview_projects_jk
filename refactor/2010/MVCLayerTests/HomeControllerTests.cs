ce9557c121250dc7d3edbb96ebafcb9a4e5faa89
GLeibniz <development@kerryhouse.net>
2014-11-01 23:57:38 -0400

    added home controller tests to get that layer to 100% coverage.

---
+++ refactor/2010/MVCLayerTests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC.Controllers;
+using MvcContrib.TestHelper;
+
+namespace MVCLayerTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController();
+            var result = controller.Index();
+            result.AssertViewRendered();
+        }
+    }
+}
